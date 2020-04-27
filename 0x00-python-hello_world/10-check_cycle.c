a7810012c5671aaac50e1445394694c819642ffa
Yosri-ctrl <yosribouabid@gmail.com>
2020-04-27 21:21:23 +0100

    task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+*check_cycle - check if the is a cycle or not
+*@list: the list to check
+*Return: 0 if there is no cycle || 1 if exist
+*/
+int check_cycle(listint_t *list)
+{
+	listint_t *current;
+
+	current = list;
+	while(current->next != NULL)
+	{
+		if (current->next == list)
+			return (1);
+		current = current->next;
+	}
+	return (0);
+}
