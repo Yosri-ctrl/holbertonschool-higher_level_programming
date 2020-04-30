e19a28cbf1f36d617b9cbbef7f1d425a38bd7d20
Yosri-ctrl <yosribouabid@gmail.com>
2020-04-30 16:40:13 +0100

    task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 add = __import__('add_0').add
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    c = add(a, b)
+    print("{} + {} = {}".format(a, b, c))
