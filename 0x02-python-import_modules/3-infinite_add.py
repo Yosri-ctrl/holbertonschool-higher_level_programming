7ecf1c62eb6837e9e5ba668c098a61a2d61966ac
Yosri-ctrl <yosribouabid@gmail.com>
2020-04-30 18:21:54 +0100

    task 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys as s
+    somme = 0
+    for i in range (1, len(s.argv)):
+        somme += int(s.argv[i])
+    print("{}".format(somme))
