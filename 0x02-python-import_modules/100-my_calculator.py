6b84776fd492907fc5149e3cd0435ad227946958
Yosri-ctrl <yosribouabid@gmail.com>
2020-04-30 22:22:16 +0100

    task adv 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     if len(s.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    ch = {"+" : add, "-" : sub, "*" : mul, "/" : div}
+    ch = {"+": add, "-": sub, "*": mul, "/": div}
     if s.argv[2] not in ch:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
