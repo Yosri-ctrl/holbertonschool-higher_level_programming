554af6f035c9b21a45be1d082a6188b106d30e64
Yosri-ctrl <yosribouabid@gmail.com>
2020-04-30 23:17:00 +0100

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import hidden_4 as h
     ch = dir(h)
     for i in range(len(ch)):
-        if ch[0] != "_":
+        if ch[i][0] != "_":
             print("{}".format(ch[i]))
