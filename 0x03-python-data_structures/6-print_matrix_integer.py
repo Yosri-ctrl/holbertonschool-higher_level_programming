92ae3a78a787eff1ab1c23429ad9aea1b63a5f5e
Yosri-ctrl <yosribouabid@gmail.com>
2020-05-05 16:29:04 +0100

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix is None:
-        return None
+    ##if matrix is None:
+    ##    return None
     for i in matrix:
         for j in i:
             print("{:d} ".format(j), end="")
