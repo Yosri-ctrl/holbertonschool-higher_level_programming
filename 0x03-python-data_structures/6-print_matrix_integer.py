2776917d8a514cdea0da69de10610dcb384f617a
Yosri-ctrl <yosribouabid@gmail.com>
2020-05-05 16:51:29 +0100

    tas k6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    ##if matrix is None:
-    ##    return None
+    if matrix is None:
+        return None
     for i in matrix:
-        for j in i:
-            print("{:d} ".format(j), end="")
-        print()
+        print(" ".join("{:d}".format(j)for j in i))
+        ##for j in i:
+        #    print(' '.join('{:d}'.format(j)))
