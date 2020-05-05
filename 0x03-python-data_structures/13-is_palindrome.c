08a846984b33db6ac9f98b79624c2d8f400fa95a
Yosri-ctrl <yosribouabid@gmail.com>
2020-05-05 20:35:56 +0100

    task 13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+/**
+*is_palindrome - check if a list is a palindrom
+*@head: the list to check
+*Return: If palindron return 0
+*/
+int is_palindrome(listint_t **head)
+{
+    listint_t *node, *new, *first, *second;
+
+    node = *head;
+    new = NULL;
+    second = *head;
+    first = second->next;
+
+    while (first->next)
+    {
+        first = first->next;
+        second = second->next;
+    }
+
+    while (second != node)
+    {
+        add_nodeint_end(&new, first->n);
+        /*print_listint(new);*/
+        first = second;
+        second = *head;
+        while (second->next != first && second)
+            second = second->next;
+        /*print_listint(new);*/
+    }
+    add_nodeint_end(&new, first->n);
+    add_nodeint_end(&new, second->n);
+
+    while (node->next)
+    {
+        if (node->n == second->n)
+        {
+            node = node->next;
+            second = second->next;
+        }
+        else
+            return (0);
+    }
+    return (1);
+}
