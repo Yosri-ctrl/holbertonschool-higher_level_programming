f0ffc29f7bfa0a85fbc56de3e3e5b53f60a3b5a1
Yosri-ctrl <yosribouabid@gmail.com>
2020-05-05 17:40:03 +0100

    task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list == None:
+        return None
+    x = my_list[0]
+    for i in my_list:
+        if x < i:
+            x = i
+    return x
