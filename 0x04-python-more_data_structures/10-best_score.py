0d3179b12a040588d649fa24d6290690d343d7e7
Yosri-ctrl <yosribouabid@gmail.com>
2020-05-07 16:04:02 +0100

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    max = 0    
+    max = 0
+    maxi = ""
     if a_dictionary is None:
         return None
-    for i in a_dictionary.values():
-        if max < i:
-            max = i
-    return max
+    for i, j in a_dictionary.items():
+        if max < j:
+            max = j
+            maxi = i
+    return maxi
