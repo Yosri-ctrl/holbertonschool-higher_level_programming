66aa1077ed61ef6160f99654ce9c8f909e395a1e
Yosri-ctrl <yosribouabid@gmail.com>
2020-05-07 20:06:28 +0100

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string) -> int:
+def roman_to_int(roman_string):
     alpha = {"M": 1000, "D": 500, "C": 100, 
-    "L": 50, "X": 10, "V": 5, "I": 1}
+    "L": 50, "X": 10, "V": 5, "I": 1, None: None}
     sum = 0
-    for i in range(len(roman_string)):
+    for i in roman_string:
         for x, y in alpha.items():
-            #if roman_string[i] == "I":
+            #if i == "I" and j == x:
             #    sum += y - 1
                 
-            if x == roman_string[i]:
+            if x == i:
                 sum += y
     return sum
