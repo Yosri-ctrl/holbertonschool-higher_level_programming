c85fc55c82af8d66b12228b1d8223fbea9a63d6a
Yosri-ctrl <yosribouabid@gmail.com>
2020-05-07 20:54:50 +0100

    task 102

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    new = []
+    for i,j in a_dictionary.items():
+        if value == j:
+            new.append(i)
+    for i in new:
+        del a_dictionary[i]
+    return a_dictionary
