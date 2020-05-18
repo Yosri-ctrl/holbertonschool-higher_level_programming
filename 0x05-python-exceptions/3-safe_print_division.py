80c90f1b7058c39cc8b31532efd0b1d875c5d728
Yosri-ctrl <yosribouabid@gmail.com>
2020-05-18 14:27:15 +0100

    task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,8 +1,9 @@
 #!/usr/bin/pyhton3
 def safe_print_division(a, b):
+    result = 0
     try:
         result = a / b
-    except ZeroDivisionError:
+    except (ZeroDivisionError, ValueError):
         result = None
     finally:
         print("Inside result: {}".format(result))
