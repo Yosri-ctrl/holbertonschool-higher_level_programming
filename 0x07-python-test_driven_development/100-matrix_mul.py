878b308439719222fa2594752afe35f297f8067f
Yosri-ctrl <yosribouabid@gmail.com>
2020-05-22 20:50:21 +0100

    task 100

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -14,7 +14,7 @@ def matrix_mul(m_a, m_b):
     for i in m_a:
         if type(i) != list:
             raise TypeError("m_a must be a list of lists")
-        if m_a == None or i == None:
+        if len(m_a) == 0 or len(i) == 0:
             raise ValueError("m_a can't be empty")
         if len(i) != row_size_a:
             raise TypeError("each row of m_a must be of the same size")
@@ -24,14 +24,22 @@ def matrix_mul(m_a, m_b):
     for i in m_b:
         if type(i) != list:
             raise TypeError("m_b must be a list of lists")
-        if m_b == None or i == None:
+        if len(m_b) == 0 or len(i) == 0:
             raise ValueError("m_b can't be empty")
         if len(i) != row_size_b:
             raise TypeError("each row of m_b must be of the same size")
         for j in i:
             if type(j) not in [float, int]:
                 raise TypeError("m_b should contain only integers or floats")
-    
+    a = len(m_a)
+    b = len(m_a[0])
+    c = len(m_b)
+    d = len(m_b[0])
+    if b != c:
+        raise ValueError("m_a and m_b can't be multiplied")
+    new = [[0 for i in range(d)] for j in range(a)]
     for i in range(len(m_a)):
-        for j in range(len(m_a[i])):
-            print("j=", m_a[i][j])
+        for j in range(len(m_b[0])):
+            for k in range(len(m_b)):
+                new[i][j] += m_a[i][k] * m_b[k][j]
+    return new
