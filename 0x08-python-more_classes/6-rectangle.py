48e76aa7e35b820b84cde6f4bd6748e3c1acde85
Yosri-ctrl <yosribouabid@gmail.com>
2020-05-25 18:18:11 +0100

    task 6

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -7,6 +7,7 @@ Define a rectangle
 class Rectangle:
     """Define a rectangle"""
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
