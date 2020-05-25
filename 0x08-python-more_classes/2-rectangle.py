53441b85fea4c09042a476b5c964c5ee3fe862f6
Yosri-ctrl <yosribouabid@gmail.com>
2020-05-25 18:22:37 +0100

    task 2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -41,4 +41,4 @@ class Rectangle:
     def perimeter(self):
         if self.__width == 0 or self.__height == 0:
             return 0
-        return (self.__height + self.__width) * 2
+        return self.__height * 2 + self.__width * 2
