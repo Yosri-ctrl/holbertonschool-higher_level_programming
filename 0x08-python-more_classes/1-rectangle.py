46f6929d7dded0d34d4a52848a4f6122cc5f6dfc
Yosri-ctrl <yosribouabid@gmail.com>
2020-05-25 18:05:08 +0100

    task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,10 +9,11 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-        
+
     @property
     def width(self):
         return self.__width
+
     @width.setter
     def width(self, value):
         if type(value) != int:
@@ -24,6 +25,7 @@ class Rectangle:
     @property
     def height(self):
         return self.__height
+
     @width.setter
     def height(self, value):
         if type(value) != int:
