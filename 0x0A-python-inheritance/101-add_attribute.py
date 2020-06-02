373ee92b4bede562ddca3c55eaa58fc20ed84d79
Yosri-ctrl <yosribouabid@gmail.com>
2020-06-02 23:58:22 +0100

    task 13

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+add an atrribute
+"""
+
+
 def add_attribute(obj, str1, str2):
+    """
+    add str2 to str1
+    """
     test = getattr(obj, "__doc__", None)
     if test is None:
         setattr(obj, str1, str2)
