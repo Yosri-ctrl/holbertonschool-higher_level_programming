b7414a92e75556b5206de805c78a76b2ea1960d7
Yosri-ctrl <yosribouabid@gmail.com>
2020-06-02 22:28:20 +0100

    task 0]

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -5,5 +5,8 @@ print the attributs
 
 
 def lookup(obj):
+    """
+     returns the list of available attributes and methods of an object
+     """
     list = [i for i in dir(obj)]
     return list
