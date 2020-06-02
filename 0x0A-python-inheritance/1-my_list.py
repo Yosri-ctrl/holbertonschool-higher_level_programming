e28de7bad8a6d3bfeb5507e54a8cab3756509ee8
Yosri-ctrl <yosribouabid@gmail.com>
2020-06-02 22:29:53 +0100

    task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+define Mylist a subcalss for list
+"""
+
+
 class Mylist(list):
+    """
+    print a sorted list of list
+    """
     def print_sorted(self):
         print(sorted(self)
