8b36b7c633c127bbefb111b80091e630cb86c45a
Yosri-ctrl <yosribouabid@gmail.com>
2020-06-02 22:32:42 +0100

    task 3

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+check object is an instance of, or if the object is an instance of a class that inherited from
+"""
+
+
 def is_kind_of_class(obj, a_class):
+    """
+    object is an instance of, or if the object is an instance of a class that inherited from
+    """
     if issubclass(type(obj), a_class):
         return True
     return False
