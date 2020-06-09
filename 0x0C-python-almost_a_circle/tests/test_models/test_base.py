7992c5e165356515f1ba740c9b524ad668beb33c
Yosri-ctrl <yosribouabid@gmail.com>
2020-06-09 19:08:14 +0100

    tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -11,21 +11,25 @@ class TestBaseClass(u.TestCase):
     Test the Base Class
     """
     def test_id_first(self):
+        """test id """
         b1 = Base()
         a1 = b1.id
         self.assertEqual(a1, 1)
 
     def test_id_givin(self):
+        """test id """
         b4 = Base(12)
         a4 = b4.id
         self.assertEqual(a4, 12)
 
     def test_id_second(self):
+        """test id """
         b2 = Base()
         a2 = b2.id
         self.assertEqual(a2, 2)
 
     def test_id_negative(self):
+        """test id """
         b3 = Base(-5)
         a3 = b3.id
         self.assertEqual(a3, -5)
