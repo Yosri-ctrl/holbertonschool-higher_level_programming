4890a8ea985521ee213fa063cf071edb155af27e
Yosri-ctrl <yosribouabid@gmail.com>
2020-09-15 18:21:25 +0100

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-const len = process.argv.length - 2
-if (len < 1) {
+if (process.argv.length < 3) {
     console.log('No argument');
-} else if (len == 1) {
+} else if (process.argv.length === 3) {
     console.log('Argument found');
 } else {
     console.log('Arguments found');
