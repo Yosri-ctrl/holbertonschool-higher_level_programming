2165fb09aab210e91c415d17bd6bc04165e05a96
Yosri-ctrl <yosribouabid@gmail.com>
2020-09-15 20:45:27 +0100

    taks 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 if (process.argv.length <= 3) {
     console.log(0)
+} else {
+    let a = process.argv.slice(2)
+    const second = a.sort((a, b) => b - a)[1];
+    console.log(second);
 }
-let a = process.argv.slice(2)
-const second = a.sort((a, b) => b - a)[1];
-console.log(second);
