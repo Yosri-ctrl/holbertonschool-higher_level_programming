dc12cac672219f9dd210c49cfcad61d3fc8edef8
Yosri-ctrl <yosribouabid@gmail.com>
2020-09-21 20:20:41 +0100

    Task ^

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const Rectangle = require('./5-square');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    c === undefined ? c = 'x' : c = 'c';
+    this.print(c);
+  }
+
+  print (c) {
+    let display = '';
+    for (let i = 0; i < this.height; i++) {
+      for (let i = 0; i < this.width; i++) {
+        display += c;
+      }
+      if (i < this.height - 1) { display += '\n'; }
+    }
+    console.log(display);
+  }
+};
