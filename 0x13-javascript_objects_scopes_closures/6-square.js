d76fc1e9a01c6c5e5d1d6cd9079901ec40fdb91b
Yosri-ctrl <yosribouabid@gmail.com>
2020-09-21 21:00:52 +0100

    fixing loop

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,23 +2,23 @@
 const Rectangle = require('./5-square');
 
 module.exports = class Square extends Rectangle {
-    constructor(size) {
-        super(size, size);
-    }
+  constructor (size) {
+    super(size, size);
+  }
 
-    charPrint(c) {
-        if (c === undefined) { c = 'X'; }
-        this.print(c);
-    }
+  charPrint (c) {
+    if (c === undefined) { c = 'X'; }
+    this.print(c);
+  }
 
-    print(c) {
-        let display = '';
-        for (let i = 0; i < this.height; i++) {
-            for (let i = 0; i < this.width; i++) {
-                display += c;
-            }
-            if (i < this.height - 1) { display += '\n'; }
-        }
-        console.log(display);
+  print (c) {
+    let display = '';
+    for (let i = 0; i < this.height; i++) {
+      for (let i = 0; i < this.width; i++) {
+        display += c;
+      }
+      if (i < this.height - 1) { display += '\n'; }
     }
+    console.log(display);
+  }
 };
