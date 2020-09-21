efa24b561a830f0e167aa2ed187c90607344e29c
Yosri-ctrl <yosribouabid@gmail.com>
2020-09-21 20:21:23 +0100

    fix semistandard

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,21 +1,21 @@
 #!/usr/bin/node
 
 module.exports = class Rectangle {
-    constructor(w, h) {
-        if (w > 0 && h > 0) {
-            this.width = w;
-            this.height = h;
-        }
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
+  }
 
-    print() {
-        let display = ""
-        for (let i = 0; i < this.height; i++) {
-            for (let i = 0; i < this.width; i++) {
-                display += "x";
-            }
-            if (i < this.height - 1) { display += "\n"; }
-        }
-        console.log(display);
+  print () {
+    let display = '';
+    for (let i = 0; i < this.height; i++) {
+      for (let i = 0; i < this.width; i++) {
+        display += 'x';
+      }
+      if (i < this.height - 1) { display += '\n'; }
     }
-}
+    console.log(display);
+  }
+};
