2ab76bb9f70d71d51ddff393d4d9bcc170d1c851
Yosri-ctrl <yosribouabid@gmail.com>
2020-09-22 21:16:22 +0100

    fixing semistanderd

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const fs = require("fs");
+const fs = require('fs');
 
-fs.readFile(process.argv[2], "utf8", (err, file) => {
+fs.readFile(process.argv[2], 'utf8', (err, file) => {
   if (err) {
     return console.log(err);
   }
