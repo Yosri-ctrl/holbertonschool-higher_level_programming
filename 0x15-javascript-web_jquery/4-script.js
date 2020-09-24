bf67b16e35f2ef2ef381eafc412d2bf0ed44f1aa
Yosri-ctrl <yosribouabid@gmail.com>
2020-09-24 22:29:31 +0100

    finished tasks

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,9 @@
+$("DIV#toggle_header").on("click", function () {
+  $("header").toggleClass(function () {
+    if ($(this).hasClass("red")) {
+      return "green";
+    } else {
+      return "red";
+    }
+  });
+});
