7881b33a71f4f51fa523fb0d8106a384c387b805
Yosri-ctrl <yosribouabid@gmail.com>
2020-09-24 23:00:14 +0100

    semistander

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
-const request = require("request");
+const request = require('request');
 
-let dic = {};
+const dic = {};
 request(process.argv[2], (err, res, body) => {
   if (err) {
     return console.log(err);
   }
 
-  let content = JSON.parse(body);
+  const content = JSON.parse(body);
 
   for (let i = 0; i < content.length; i++) {
-    let user = content[i].userId;
+    const user = content[i].userId;
     if (content[i].completed) {
       dic[user] = (dic[user] || 0) + 1;
     }
