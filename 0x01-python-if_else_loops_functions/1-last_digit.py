4654b00f8ea7e8e013b131ffd4fc835de2f986fa
Yosri-ctrl <yosribouabid@gmail.com>
2020-11-04 18:40:46 +0100

    changing variable name

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-x = number % 10
-if x > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, x))
-elif x is 0:
-    print("Last digit of {} is {} and is 0".format(number, x))
-elif x < 6 and x != 0:
+digit = number % 10
+if digit > 5:
+    print("Last digit of {} is {} and is greater than 5".format(number, digit))
+elif digit is 0:
+    print("Last digit of {} is {} and is 0".format(number, digit))
+elif digit < 6 and digit != 0:
     print("Last digit of {} is {} \
-and is less than 6 and not 0".format(number, x))
+and is less than 6 and not 0".format(number, digit))
